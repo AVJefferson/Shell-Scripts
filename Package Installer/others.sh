bdc99fc17ebd7ad2f876c4663629087a034e1585
A V Jefferson <avjeferson@gmail.com>
2021-11-13 23:59:54 +0530

    Create others.sh

---
+++ Package Installer/others.sh
@@ -0,0 +1,22 @@
+sudo apt install apt-transport-https curl wget software-properties-common
+
+
+# BRAVE
+sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
+
+echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+
+sudo apt update
+sudo apt install brave-browser
+
+
+# VS CODE
+wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
+
+sudo apt update
+sudo apt install code
+
+
+
+sudo apt upgrade
